31c1e6c9d0642b57e1746e6be94389cc259854bb
Jeammm <athicha.phjkl@gmail.com>
2023-03-04 20:57:26 +0700

    50 [ [PPPP][PTPTPPPTTTTP] ]

--- week_2/chains.cpp
+++ week_2/chains.cpp
@@ -1,158 +1,118 @@
 #include <iostream>
-// #include <vector>
-#include <cstdlib>
+#define MAX_N 100010
 
 using namespace std;
-// vector<int> path;
-int path[100001];
-int path_index = 0;
 
-typedef struct list {
-    int data;
-    struct list *next;
-    struct list *prev;
-} list_t;
+int l, n;
+int dna[MAX_N];
+int parent[MAX_N];
+int child[MAX_N];
+int count = 0;
+int current = 0;
 
-list_t * append(list_t* list, int code) {
-    list_t* line = (list_t*)malloc(sizeof(line));
-    line->data = code;
-    line->next = NULL;
-    line->prev = NULL;
-    if (list == NULL) {
-        return line;
-    } else {
-        list_t* temp = list;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = line;
-        line->prev = temp;
-        return list;
-    }
-}
+void cut(int target)
+{
+    parent[child[current]] = -1;  // cut child
 
-void reverse_list(list_t *&list) {
-  list_t *temp = NULL;
-  list_t *current = list;
-
-  while (current != NULL) {
-    temp = current->prev;
-    current->prev = current->next;
-    current->next = temp;
-    current = current->prev;
-  }
-
-  if (temp != NULL) {
-    list = temp->prev;
-  }
-}
-
-list_t * swap(list_t* a, list_t* b, list_t* robot, int reverse) {
-    if (reverse == 1) {
-        reverse_list(b);
-    }
-    // cout << "robot: " << robot->data << "\n";
-    list_t *temp = robot->next;
-    if (temp != NULL) {
-        temp->prev = NULL;
-    }
-
-    b->prev = robot;
-    robot->next = b;
-    return temp;
-}
-
-int front_list(list_t* line) {
-    return line->data;
-}
-
-int back_list(list_t* line) {
-    while (line->next != NULL) {
-        line = line->next;
-    }
-    return line->data;
-}
-
-int print_list(list_t* list) {
-    while (list != NULL) {
-        cout << list->data << " ";
-        list = list->next;
-    }
-    cout << "\n";
-    return 1;
-}
-
-int register_path(list_t* robot) {
-    path[path_index] = robot->data;
-    // path.push_back(robot->data);
-    path_index++;
-    return 1;
-}
-
-int main(void) {
-    int l, n;
-    cin >> l >> n;
-    list_t** storage = (list_t**)malloc(sizeof(storage) * l);
-    int current = 1;
-    for (int i=0; i<l; i++) {
-        int length;
-        cin >> length;
-        list_t* line = NULL;
-        for (int k=0; k<length; k++) {
-            line = append(line, current);
-            current++;
-        }
-        storage[i] = line;
-    }
-
-    list_t *robot = storage[0];
-    for (int i=0; i<n; i++) {
-        char cmd;
-        cin >> cmd;
-        if (cmd == 'F') {
-            if (robot->next != NULL) {
-                robot = robot->next;
-            }
-        } else if (cmd == 'B') {
-            if (robot->prev != NULL) {
-                robot = robot->prev;
-            }
-        } else if (cmd == 'C') {
-            int x;
-            cin >> x;
-            int target = 0;
-            int reverse = 0;
-            for (int i=1; i<l; i++) {
-                if (front_list(storage[i]) == x) {
-                    target = i;
-                    break;
-                }
-            }
-            if (target == 0) {
-                for (int i=1; i<l; i++) {
-                    if (back_list(storage[i]) == x) {
-                        reverse = 1;
-                        target = i;
-                        break;
-                    }
-                }
-            }
-
-            storage[target] = swap(storage[0], storage[target], robot, reverse);
-            if (robot->next != NULL) {
-                robot = robot->next;
-            }
-        }
+    if (child[target] == -1) // reverse
+    {
+        child[target] = current;
         
-        register_path(robot);
- 
-        // cout << "line";
-        // print_list(storage[0]);
-    // }
-    // for (auto ptr : path) {
-    //     cout << ptr << "\n";
+        int it = target;
+        while (parent[it] != -1)
+        {
+            int temp_parent = parent[it];
+            parent[it] = child[it];
+            child[it] = temp_parent;
+            it = child[it];
+        }
+        int temp_parent = parent[it];
+        parent[it] = child[it];
+        child[it] = temp_parent;
+        it = child[it];
     }
-    for (int i=0; i<path_index; i++) {
-        cout << path[i] << "\n";
+    else // not reverse
+    {
+        parent[target] = current;
     }
+    child[current] = target;
+    current = child[current];
+
+}
+
+void get_input()
+{
+    
+    cin >> l >> n;
+    for (int i = 0; i < l; i++)
+    {
+        int line;
+        cin >> line;
+
+        for (int j = 0; j < line; j++)
+        {   
+            dna[count] = count + 1;
+
+            if (line == 1)
+            {
+                parent[count] = -1;
+                child[count] = -1;
+            }
+            else if (j == 0)
+            {
+                parent[count] = -1;
+                child[count] = count + 1;
+            }
+            else if (j == line - 1)
+            {
+                parent[count] = count - 1;
+                child[count] = -1;
+            }
+            else
+            {
+                parent[count] = count - 1;
+                child[count] = count + 1;
+            }
+
+            count++;
+        }
+    }
+}
+
+void move()
+{
+    for (int i = 0; i < n; i++)
+    {
+        char cmd;
+        int target;
+        cin >> cmd;
+        switch (cmd)
+        {
+        case 'F':
+            if (child[current] != -1)
+            {
+                current = child[current];
+            }
+            break;
+        case 'B':
+            if (parent[current] != -1)
+            {
+                current = parent[current];
+            }
+            break;
+        case 'C':
+            cin >> target; target--;
+            cut(target);
+            break;
+        }
+        cout << dna[current] << "\n";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    get_input();
+    move();
 }
