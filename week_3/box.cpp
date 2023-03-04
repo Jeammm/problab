c2e4000f04aee98d22a3e6f5bffacac1450d8220
Jeammm <athicha.phjkl@gmail.com>
2023-03-04 23:01:09 +0700

    100 [ PPPPPPPPPPPP ]

--- week_3/box.cpp
+++ week_3/box.cpp
@@ -1,76 +1,83 @@
 #include <iostream>
-#include <vector>
 #define MAX_N 35
 
 using namespace std;
-
 
 int map[MAX_N][MAX_N] = {0};
 bool seen[MAX_N][MAX_N] = {false};
 bool possible = false;
 int n, m;
 
-
-int check_space(int x, int y) {
-    if (y < 0 || y >= n-1 || x < 0 || x >= m-1) {
+int check_space(int x, int y)
+{
+    if (y < 0 || x < 0 || x >= m - 1)
+    {
         return 0;
     }
-    if (map[y][x] != 0 || map[y+1][x] != 0 || map[y][x+1] != 0 || map[y+1][x+1] != 0) {
+    if (map[y][x] != 0 || map[y + 1][x] != 0 || map[y][x + 1] != 0 || map[y + 1][x + 1] != 0)
+    {
         return 0;
     }
 
     return 1;
 }
 
-void move(int x, int y) {
-    
-    if (possible){
+void move(int x, int y)
+{
+
+    if (possible)
+    {
         return;
     }
 
-    if (seen[y][x]){
+    if (seen[y][x])
+    {
         return;
     }
 
-    if (check_space(x, y) == 0){
+    if (check_space(x, y) == 0)
+    {
         return;
     }
 
-    if (y == n-2){
+    if (y == n - 2)
+    {
         possible = true;
         return;
     }
 
     seen[y][x] = true;
 
-    move(x, y+1);
-    move(x+1, y);
-    move(x-1, y);
-    move(x, y-1);
+    move(x, y + 1);
+    move(x, y - 1);
+    move(x + 1, y);
+    move(x - 1, y);
 
     return;
 }
 
-int main(void) {
+int main(void)
+{
     cin >> n >> m;
-    for (int i=0 ;i<n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         string read;
         cin >> read;
-        for (int j=0; j<m; j++) {
+        for (int j = 0; j < m; j++)
+        {
             map[i][j] = (read[j] == '#');
         }
     }
-    
-    for (int i=0; i<m-1; i++) {
-        if (check_space(i, 0)) {
-            move(i, 0);
-            if (possible) {
-                cout << "yes" << "\n";
-                return 1;
-            }
+
+    for (int i = 0; i < m - 1; i++)
+    {
+        move(i, 0);
+        if (possible)
+        {
+            cout << "yes";
+            return 0;
         }
     }
-    cout << "no" << "\n";
-    return 0;
-    
+
+    cout << "no";
 }
