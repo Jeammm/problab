4205a3c277f8a68289ff701817bed670711981ec
Jeammm <athicha.phjkl@gmail.com>
2023-03-05 00:40:05 +0700

    100 [ PPPPP[PP][PP][PP][PP][PP] ]

--- week_4+5/shipmentcost.cpp
+++ week_4+5/shipmentcost.cpp
@@ -8,16 +8,22 @@ const int MAX_N = 100010;
 
 int m, n, s, t;
 vector<int> adj[MAX_N];
+int min_cost[MAX_N];
 int deg[MAX_N];
-int count = 0;
-bool storage[MAX_N];
+
+bool is_storage[MAX_N];
+bool is_factory[MAX_N];
+vector<int> storage;
 vector<int> factory;
 
 void read_input()
 {
   cin >> n >> m >> s >> t;
   for (int i = 0; i < n; i++)
+  {
     deg[i] = 0;
+    min_cost[i] = -1;
+  }
 
   for (int i = 0; i < m; i++)
   {
@@ -36,15 +42,16 @@ void read_input()
   {
     int inp;
     cin >> inp; inp--;
-    storage[inp] = true;
+    is_storage[inp] = true;
+    storage.push_back(inp);
   }
 
   for (int i = 0; i < t; i++)
   {
     int inp;
     cin >> inp; inp--;
+    is_factory[inp] = true;
     factory.push_back(inp);
-    count++;
   }
 
 
@@ -59,10 +66,11 @@ void init()
   {
     seen[u] = false;
     layer[u] = -1;
+    
   }
 }
 
-int bfs(int s)
+void bfs_f(int s, int it)
 {
   list<int> Q;
 
@@ -74,9 +82,10 @@ int bfs(int s)
   {
     int u = Q.front();
 
-    if (storage[u] == true)
+    if (is_storage[u] == true)
     {
-      return layer[u];
+      min_cost[it] = layer[u];
+      return;
     }
 
     Q.pop_front();
@@ -93,14 +102,71 @@ int bfs(int s)
   }
 }
 
+
+void bfs_s(int s)
+{
+  list<int> Q;
+
+  Q.push_back(s);
+  seen[s] = true;
+  layer[s] = 0;
+
+  while (!Q.empty())
+  {
+    int u = Q.front();
+
+    Q.pop_front();
+    for (int d = 0; d < deg[u]; d++)
+    {
+      int v = adj[u][d];
+      if (!seen[v])
+      {
+        Q.push_back(v);
+        seen[v] = true;
+        layer[v] = layer[u] + 1;
+      }
+    }
+  }
+}
+
+void print_min_cost()
+{
+  for (int i = 0; i < t; i++)
+  {
+    cout << min_cost[i] << "\n";
+  }
+  // cout << "\n";
+}
+
 int main()
 {
   read_input();
-  for (int i = 0; i < count; i++)
+
+  if (t < s)
   {
-    init();
-    cout << bfs(factory[i]) << "\n";
+    for (int i = 0; i < t; i++)
+    {
+      init();
+      bfs_f(factory[i], i);
+    }
   }
+  else
+  {
+    for (int i = 0; i < s; i++)
+    {
+      init();
+      bfs_s(storage[i]);
+      for (int j = 0; j < t; j++)
+      {
+        if (min_cost[j] == -1 || layer[factory[j]] < min_cost[j])
+        {
+          min_cost[j] = layer[factory[j]];
+        }
+      }
+    }
+    
+  }
+  print_min_cost();
 
   // init();
   // bfs(0);
